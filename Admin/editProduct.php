f28b724416043f563d0280a41e50a19823cf66c3
Faraz-Inam <faraz_inam@aptechnorth.edu.pk>
2024-11-24 18:53:44 +0500

    Update Product with image and Delete Product WORK

--- Admin/editProduct.php
+++ Admin/editProduct.php
@@ -1,97 +1,89 @@
-<?php 
+<?php
 include("header.php");
 include("connection.php");
 
-
 if(isset($_GET['i'])){
     $p_id = $_GET['i'];
-    $sel_pr = "SELECT p.*, c.category_name, b.brand_name
-    FROM `products` p
-    INNER JOIN `categories` c
-    ON p.category_id = c.category_id
-    INNER JOIN `brands` b
-    ON p.brand_id = b.brand_id WHERE product_id = '$p_id'";
 
-    $sel_query = mysqli_query($connect, $sel_pr);
-    $fetch_pr = mysqli_fetch_assoc($sel_query);
-    ?>
+$select = "SELECT p.*, category_name, brand_name
+FROM products p
+INNER JOIN categories c
+ON c.category_id = p.category_id
+INNER JOIN brands b
+ON b.brand_id = p.brand_id WHERE product_id = '$p_id'";
+$query = mysqli_query($connect, $select);
+$fetch_pr = mysqli_fetch_assoc($query);
+?>
 
-    <!-- Form Start -->
-    <div class="container-fluid pt-4 px-4">
-        <div class="row g-4">
-           <form action="" method="post" enctype="multipart/form-data">
+<!-- Form Start -->
+<form action="" method="post" enctype="multipart/form-data">
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Update Product</h6>
 
-            <div class="col-sm-12 col-xl-6">
-                <div class="bg-light rounded h-100 p-4">
-                    <h6 class="mb-4">Select</h6>
-                   
-                    <select name="c_id" class="form-select mb-3" aria-label="Default select example">
-                        <option <?php echo $fetch_pr['category_id']?>> <?php echo $fetch_pr['category_name']?> </option>
-                        <?php
-                        $cat = "SELECT * FROM categories";
-                        $cat_q = mysqli_query($connect, $cat);
-                        while($option = mysqli_fetch_assoc($cat_q)){ ?>
-                            <option value=" <?php echo $option['category_id']?> "> <?php echo $option['category_name']?> </option>
-                     <?php   }
-                        ?>
-                        
-                    </select>
+                            <select name="c_id" class="form-select mb-3" aria-label="Default select example">
+                                <!-- <option value="<?php echo $fetch_pr['category_id'] ?>"> <?php echo $fetch_pr['category_name'] ?></option> -->
+                                <?php
+                                $sel_cat = "SELECT * FROM categories";
+                                $query_cat = mysqli_query($connect, $sel_cat);
+                                while($fetch_cat = mysqli_fetch_assoc($query_cat)){ ?>
+                 
+                                <option value="<?php echo $fetch_cat['category_id'] ?>" <?php if($fetch_pr['category_id'] == $fetch_cat['category_id']){echo "selected = 'selected'";} ?>> <?php echo $fetch_cat['category_name'] ?> </option>
 
-                    <select name="b_id" class="form-select mb-3" aria-label="Default select example">
-                        <option <?php echo $fetch_pr['brand_id']?>><?php echo $fetch_pr['brand_name']?></option>
-                        <?php
-                         $brand = "SELECT * FROM brands";
-                         $brand_q = mysqli_query($connect, $brand);
-                        while($option = mysqli_fetch_assoc($brand_q)){ ?>
-                            <option value=" <?php echo $option['brand_id']?> "> <?php echo $option['brand_name']?> </option>
-                     <?php   }
-                        ?>
+                              <?php  }?>
+                               
+                            </select>
 
-                    </select>
+                            <select name="b_id" class="form-select mb-3" aria-label="Default select example">
+                                 <!-- <option value="<?php echo $fetch_pr['brand_id'] ?>"> <?php echo $fetch_pr['brand_name'] ?></option> -->
+                                <?php
+                                $sel_brand = "SELECT * FROM brands";
+                                $query_brand = mysqli_query($connect, $sel_brand);
+                                while($fetch_brand = mysqli_fetch_assoc($query_brand)){ ?>
+                 
+                                <option value="<?php echo $fetch_brand['brand_id'] ?>" <?php if($fetch_pr['brand_id'] == $fetch_brand['brand_id']){ echo "selected = 'selected'";} ?>> <?php echo $fetch_brand['brand_name'] ?> </option>
 
-                    <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Product Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" name="p_name" value="<?php echo $fetch_pr['product_name'] ?>"
-                                aria-describedby="emailHelp">
+
+                              <?php  }?>
+                               
+                            </select>
+                            
+                            <label for="pn" class="form-label">Product Name</label>
+                                    <input type="text" name="p_name" value="<?php echo $fetch_pr['product_name'] ?>" class="form-control" id="pn"
+                                        aria-describedby="emailHelp">
+
+                                        <label for="pp" class="form-label">Product Price</label>
+                                    <input type="text" name="p_price" value="<?php echo $fetch_pr['product_price'] ?>" class="form-control" id="pp"
+                                        aria-describedby="emailHelp">
+
+                                        <label for="pm" class="form-label">Product Model</label>
+                                    <input type="text" name="p_model" value="<?php echo $fetch_pr['product_model'] ?>" class="form-control" id="pm"
+                                        aria-describedby="emailHelp">
+
+                                        <label for="ps" class="form-label">Product Specification</label>
+                                    <input type="text" name="p_spec" value="<?php echo $fetch_pr['product_specification'] ?>" class="form-control" id="ps"
+                                        aria-describedby="emailHelp">
+
+                                        <label for="pi" class="form-label">Product Image</label>
+                                    <input type="file" name="p_image" class="form-control" id="pi"
+                                        aria-describedby="emailHelp">
+
+                                        <img src="product_images/<?php echo $fetch_pr['product_image'] ?>" alt="">
+
+                            <button type="submit" name="update_product" class="btn btn-primary m-2">Update</button>
+                            
                         </div>
-
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Product Price</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" name="p_price" value="<?php echo $fetch_pr['product_price'] ?>"
-                                aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Product Model</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" name="p_model" value="<?php echo $fetch_pr['product_model'] ?>"
-                                aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Product Specification</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" name="p_spec" value="<?php echo $fetch_pr['product_specification'] ?>"
-                                aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Product image</label>
-                            <input type="file" class="form-control" id="exampleInputEmail1" name="p_image"
-                                aria-describedby="emailHelp">
-                        </div>
-
-                        <img src="product_images/<?php echo $fetch_pr['product_image']?>" alt="" width="100px">
-                    
-                        <button type="submit" class="btn btn-primary" name="update_btn">Update</button> 
+                    </div>
                 </div>
             </div>
-
             </form>
-        </div>
-    </div>
-    <!-- Form End -->
- <?php } 
- 
- if(isset($_POST['update_btn'])){
+            <!-- Form End -->
+<?php 
+// } 
+
+if(isset($_POST['update_product'])){
     $c_id = $_POST['c_id'];
     $b_id = $_POST['b_id'];
     $p_name = $_POST['p_name'];
@@ -100,20 +92,60 @@ if(isset($_GET['i'])){
     $p_spec = $_POST['p_spec'];
 
     $p_image = $_FILES['p_image'];
+    $img_name = $p_image['name'];
+    $img_tmpname = $p_image['tmp_name'];
+    $img_size = $p_image['size'];
+    $img_type = $p_image['type'];
 
-   $update  =  "UPDATE `products` SET `product_name`='$p_name',`product_price`='$p_price',`product_model`='$p_model',`product_specification`='$p_spec',`category_id`='$c_id',`brand_id`='$b_id' WHERE product_id = '$p_id'";
-   $done = mysqli_query($connect, $update);
-    if($done){
-        echo "<script>
-        alert('Product Updated!');
-        window.location.href = 'viewProduct.php';
-        </script>";
-       }
+    $path = 'product_images/' . $img_name;
+
+    if(is_uploaded_file($img_tmpname)){
+        move_uploaded_file($img_tmpname,$path);
+
+        $update = "UPDATE `products` SET `product_name`='$p_name',`product_price`='$p_price',`product_model`='$p_model',`product_specification`='$p_spec', `product_image` = '$img_name', `category_id`='$c_id',`brand_id`='$b_id' WHERE `product_id` = '$p_id'";
+        $done = mysqli_query($connect, $update);
+        if($done){
+            echo
+            "<script>
+            alert('Product Updated With Image!');
+            window.location.href = 'viewProduct.php';
+            </script>";
+        }
+    }
+    else{
+        $update = "UPDATE `products` SET `product_name`='$p_name',`product_price`='$p_price',`product_model`='$p_model',`product_specification`='$p_spec', `category_id`='$c_id',`brand_id`='$b_id' WHERE `product_id` = '$p_id'";
+        $done = mysqli_query($connect, $update);
+        if($done){
+            echo
+            "<script>
+            alert('Product Updated Without Image!');
+            window.location.href = 'viewProduct.php';
+            </script>";
+        }
+    }
 }
- ?>
+}
 
 
 
-<?php 
+// DELETE CODE 
+if(isset($_GET['j'])){
+    $p_id = $_GET['j'];
+
+
+$delete = "DELETE FROM products WHERE product_id = '$p_id'";
+$done = mysqli_query($connect, $delete);
+if($done){
+    echo
+    "<script>
+    alert('Product Deleted!');
+    window.location.href = 'viewProduct.php';
+    </script>";
+}
+}
+
+?>
+
+<?php
 include("footer.php");
 ?>
